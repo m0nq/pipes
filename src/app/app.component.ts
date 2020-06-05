98093e7ee07ff3b4acc8a897f8277b960b1aa0fb
m0nq <forgetmenaught@gmail.com>
2020-06-05 13:54:53 -0700

    Add ng bootstrap module and pipes to project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'cq-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'cq-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+    name: string;
+    date: string;
+
+    public onNameChange(value: string): void {
+        this.name = value;
+    }
+
+    public onDateChange(value: string) {
+        this.date = value;
+    }
 }
